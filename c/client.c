657c31c8dea49f1a355883cad8917ee6d2f13075
simondlevy <simon.d.levy@gmail.com>
2018-05-07 12:35:10 -0400

    More C

--- c/client.c
+++ c/client.c
@@ -1,11 +1,21 @@
 /*
 
    Example client program for sockets
+
+   Adapted from http://web.eecs.utk.edu/~huangj/cs360/360/notes/Sockets/socketfun.c
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
 
 int main(int argc, char ** argv)
 {
+    if (argc < 3) {
+        fprintf(stderr, "Usage:   %s <HOST> <PORT>\n", argv[0]);
+        fprintf(stderr, "Example: %s 137.113.118.3 20000\n", argv[0]);
+        exit(1);
+    }
+
     return 0;
 }
--- c/server.c
+++ c/server.c
@@ -4,8 +4,16 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
 
 int main(int argc, char ** argv)
 {
+    if (argc < 3) {
+        fprintf(stderr, "Usage:   %s <HOST> <PORT>\n", argv[0]);
+        fprintf(stderr, "Example: %s 137.113.118.3 20000\n", argv[0]);
+        exit(1);
+    }
+
     return 0;
 }
