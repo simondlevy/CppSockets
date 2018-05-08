6d7fbf48624aa12126b8a6e64cb226840c72416c
simondlevy <simon.d.levy@gmail.com>
2018-05-08 01:36:20 -0400

    C example working

--- c/client.c
+++ c/client.c
@@ -3,7 +3,7 @@
    Example client program for sockets
 
    Adapted from http://web.eecs.utk.edu/~huangj/cs360/360/notes/Sockets/socketfun.c
-*/
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,6 +18,31 @@ int main(int argc, char ** argv)
         fprintf(stderr, "Example: %s 137.113.118.3 20000\n", argv[0]);
         exit(1);
     }
+
+    char * hn = argv[1];
+    int port = atoi(argv[2]);
+    char * un = getenv("USER");
+
+    int fd = request_connection(hn, port);
+
+    printf("Connection established.  Receiving:\n\n");
+
+    FILE * fin = fdopen(fd, "r");
+    FILE * fout = fdopen(fd, "w");
+
+    // Read from the server.
+
+    char s[100];
+    if (fgets(s, 1000, fin) == NULL) {
+        printf("Server closed prematurely\n");
+        exit(0);
+    }
+    printf("Received: %s", s);
+
+    // And send a line to the server.
+
+    printf("Sending `Client: %s' to the server\n", un);
+    fprintf(fout, "Client: %s\n", un);
 
     return 0;
 }
--- c/server.c
+++ c/server.c
@@ -1,21 +1,53 @@
 /*
 
    Example server program for sockets
-*/
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
+#include <unistd.h>
 
 #include "sockettome.h"
 
 int main(int argc, char ** argv)
 {
-    if (argc < 3) {
-        fprintf(stderr, "Usage:   %s <HOST> <PORT>\n", argv[0]);
-        fprintf(stderr, "Example: %s 137.113.118.3 20000\n", argv[0]);
+    if (argc < 2) {
+        fprintf(stderr, "Usage:   %s <HOST>\n", argv[0]);
+        fprintf(stderr, "Example: %s 20000\n", argv[0]);
         exit(1);
     }
+
+    int port = atoi(argv[1]);
+    if (port < 5000) {
+        fprintf(stderr, "       port must be > 5000\n");
+        exit(1);
+    }
+
+    // Serve the socket and accept the connection.
+
+    int sock = serve_socket(port);
+    int fd = accept_connection(sock);
+
+    // Send the username along the socket.
+
+    char * un = getenv("USER");
+    printf("Connection established.  Sending `Server: %s'\n", un);
+    char s[100];
+    sprintf(s, "Server: %s\n", un);
+    write(fd, s, strlen(s));
+
+    // Read a line of text, one character at a time.
+    printf("Receiving:\n\n");
+    char c = 0;
+    do {
+        if (read(fd, &c, 1) != 1) {
+            printf("Socket Closed Prematurely\n");
+            exit(0);
+        } else putchar(c);
+    } while (c != '\n');
+
 
     return 0;
 }
