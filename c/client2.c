41c3b3777413574e67415ed7623aff650346a899
simondlevy <simon.d.levy@gmail.com>
2018-05-12 22:03:46 -0400

    Non-blocking read from server

--- c/client2.c
+++ c/client2.c
@@ -10,6 +10,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
+#include <fcntl.h>
 #include <sys/socket.h>
 
 #include "sockettome.h"
@@ -31,6 +32,9 @@ int main(int argc, char ** argv)
 
     int sockfd = 0;
 
+    FILE * fin = NULL;
+    //FILE * fout = NULL;
+
     while (true) {
 
         struct timespec ts;
@@ -46,105 +50,25 @@ int main(int argc, char ** argv)
                 sockfd = request_connection(hn, port, false);
             }
 
+            else if (fin == 0) {
+                int flags = fcntl(sockfd, F_GETFL, 0);
+                fcntl(sockfd, F_SETFL, flags|O_NONBLOCK);
+                fin  = fdopen(sockfd, "r");
+                //fout = fdopen(sockfd, "w");
+            }
+
             // connected
             else {
-                printf("%d\n", sockfd);
+
+                char s[80];
+                if (fgets(s, 80, fin) != NULL) {
+                    printf("Server said: %s\n", s);
+                }
             }
 
             prevtime = currtime;
         }
     }
 
-    /*
-       int fd = request_connection(hn, port);
-
-       printf("Connection established.  Receiving:\n\n");
-
-       FILE * fin = fdopen(fd, "r");
-       FILE * fout = fdopen(fd, "w");
-
-    // Read from the server.
-
-    char s[100];
-    if (fgets(s, 1000, fin) == NULL) {
-    printf("Server closed prematurely\n");
-    exit(0);
-    }
-    printf("Received: %s", s);
-
-    // And send a line to the server.
-
-    printf("Sending `Client: %s' to the server\n", un);
-    fprintf(fout, "Client: %s\n", un);
-     */
-
     return 0;
 }
-
-/*
-   RATE = 1 # Update frequency in Herz 
-
-   import socket
-   import time
-   import fcntl
-   import os
-
-   from hostport import hostport
-
-   if __name__ == '__main__':
-
-   messages = ['one', 'two', 'three', 'four', 'five']
-   messageId = 0
-
-   host, port = hostport()
-
-   sock = socket.socket()
-
-   connected = False
-
-   start = time.time()
-   prev = start
-
-   while True:
-
-   curr = time.time()
-
-   if curr - prev > 1./RATE:
-
-   prev = curr
-
-   if connected:
-
-try:
-msg = sock.recv(80) # Maximum number of bytes we expect
-if len(msg) < 1:
-sock.close()
-sock = socket.socket()
-connected = False
-else:
-print('Server said: ' + msg.decode('utf-8')) # Python 3 requires decoding
-
-except:
-continue
-
-try:
-sock.send(messages[messageId].encode('utf-8'))
-
-except socket.error:
-continue
-
-messageId = (messageId + 1) % len(messages)
-
-else:
-
-try:
-sock.connect((host, port)) # Note tuple!
-fcntl.fcntl(sock, fcntl.F_SETFL, os.O_NONBLOCK)
-connected = True
-print('Connected to server %s:%d' % (host, port))
-except socket.error: 
-                    continue
-
-
-    sock.close()
-    */
