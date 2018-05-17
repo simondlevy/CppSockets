537712efae3f8687b3fa23e12da57f3e42bbd26c
simondlevy <simon.d.levy@gmail.com>
2018-05-16 23:19:08 -0400

    =Cleanup

--- c/unix/server2.cpp
+++ c/unix/server2.cpp
@@ -3,10 +3,6 @@
    Example threaded server program for sockets
  */
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
