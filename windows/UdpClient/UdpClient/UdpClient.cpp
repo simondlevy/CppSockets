3d3ee52766ffac19ac5ee1dee0a241c9a240d985
unknown <simondlevy>
2019-04-02 14:46:20 +0300

    More factoring

--- windows/UdpClient/UdpClient/UdpClient.cpp
+++ windows/UdpClient/UdpClient/UdpClient.cpp
@@ -67,32 +67,7 @@ int main(void)
     char buf[BUFLEN];
     char message[BUFLEN];
 
-    /*
-
-    //Initialise winsock
-    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
-    {
-        printf("Failed. Error Code : %d", WSAGetLastError());
-        exit(EXIT_FAILURE);
-    }
-
-    //create socket
-    if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == SOCKET_ERROR)
-    {
-        printf("socket() failed with error code : %d", WSAGetLastError());
-        exit(EXIT_FAILURE);
-    }
-
-    //setup address structure
-    memset((char *)&si_other, 0, sizeof(si_other));
-    si_other.sin_family = AF_INET;
-    si_other.sin_port = htons(PORT);
-    //si_other.sin_addr.S_un.S_addr = inet_addr(SERVER);
-    InetPton(AF_INET, _T(SERVER), &si_other.sin_addr.s_addr);
-    */
-
     UdpSocketClient client;
-
 
     //start communication
     while (true)
@@ -100,22 +75,11 @@ int main(void)
         printf("Enter message : ");
         gets_s(message);
 
-        //send the message
-        if (sendto(client._s, message, strlen(message), 0, (struct sockaddr *) &client._si_other, client._slen) == SOCKET_ERROR)
-        {
-            printf("sendto() failed with error code : %d", WSAGetLastError());
-            exit(EXIT_FAILURE);
-        }
+        client.sendData(message, strlen(message));
 
-        //receive a reply and print it
-        //clear the buffer by filling null, it might have previously received data
         memset(buf, '\0', BUFLEN);
-        //try to receive some data, this is a blocking call
-        if (recvfrom(client._s, buf, BUFLEN, 0, (struct sockaddr *) &client._si_other, &client._slen) == SOCKET_ERROR)
-        {
-            printf("recvfrom() failed with error code : %d", WSAGetLastError());
-            exit(EXIT_FAILURE);
-        }
+
+        client.receiveData(buf, BUFLEN);
 
         puts(buf);
     }
