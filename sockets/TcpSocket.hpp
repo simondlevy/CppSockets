82c231424d924100f5b0357f4568155976418e15
Your Name <you@example.com>
2019-06-20 15:39:09 +0300

    =More header-only support

--- sockets/TcpSocket.hpp
+++ sockets/TcpSocket.hpp
@@ -8,7 +8,7 @@
 
 #pragma once
 
-#include "SocketCompat.h"
+#include "SocketCompat.hpp"
 
 class TcpSocket : public Socket {
 
--- sockets/UdpSocket.hpp
+++ sockets/UdpSocket.hpp
@@ -8,7 +8,7 @@
 
 #pragma once
 
-#include "SocketCompat.h"
+#include "SocketCompat.hpp"
 
 class UdpSocket : public Socket {
 
